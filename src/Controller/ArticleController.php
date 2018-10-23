1652d47031a9feb071eeb8763165d6af2dccd41e
Tadeh <tadehb@4over.com>
2018-10-22 18:49:29 -0700

    Second Commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,23 +9,80 @@
 
 namespace App\Controller;
 
+use function Sodium\add;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\type\TextareaType;
+use Symfony\Component\Form\Extension\Core\type\SubmitType;
+use App\Entity\Article;
+
 
 class ArticleController extends Controller {
 
     /**
-     * @Route("/")
+     * @Route("/",name="article_list")
      * @Method("{GET}")
      * @return Response
      */
     public function index(){
 
-        $articles=['article1','article2'];
+        $articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render("articles/index.html.twig",array('articles'=>$articles));
     }
+
+
+    /**
+     * @Route("/article/save")
+     * @Method("{GET}")
+     */
+    public function save(){
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setBody('This is the body of article one');
+        $article->setTitle('Article One');
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+
+        return $this->render("articles/index.html.twig",array('articles'=>$articles));
+
+    }
+
+
+    /**
+     * @Route("/article/{id}",name="article_show")
+     */
+    public function show($id){
+
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('article/show.html.twig',array('article'=>$article));
+
+    }
+
+    /**
+     * @Route("/article/new",name="new_article")
+     * @Method({"GET",""POST})
+     */
+
+    public function new(Request $request){
+
+        $article = new Article();
+        $form = $this->createFormBuilder($article)->add('title',TextType::class,array('attr'=>
+        array('class'=>'form-control')))->add('body',TextareaType::class,array('required'=>false,
+        'attr'=>array('class'=>'form-control')))->add('save',Submittype::class,array('label'=>'Create',
+            'attr'=>array('class'=>'btn btn-primary mt-3')))->getForm();
+
+
+    }
+
 }
 
---
+++ src/Entity/Article.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", lenght=100)
+     */
+    private $title;
+
+    /**
+     * @ORM/Column(type"text")
+     */
+    private $body;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+
+}
